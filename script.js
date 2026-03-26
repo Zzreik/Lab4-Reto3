0783ae5076d318437de4cc02b4fe1369fabb9177
josuemoma657-web <josuemoma657@gmail.com>
2026-03-26 09:50:13 -0600

    Este avance es con el conocimiento que adquiri de un curso de JavaScript, en teoria se agrego la conexion entre el html y javaScript ademas de un poco de codigo de el como creo que puede ir funcionando el JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let Nombre = "NUll";
+let Id = "000000";
+let Correo = "null@gmail.com";
+
+Nombre = document.getElementById("nombre");
+Id = document.getElementById("id");
+Correo = document.getElementById("correo");
+
+
+console.log{"Datos impresos Nombre: " +Nombre+ "Id:" +Id+ "Correo:" +Correo};
